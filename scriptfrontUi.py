38d946fd960d147e0758f19040d3434508ab063e
Pravargav <96449025+Pravargav@users.noreply.github.com>
2024-12-19 13:43:31 +0530

    Update scriptfrontUi.py

--- scriptfrontUi.py
+++ scriptfrontUi.py
@@ -30,7 +30,7 @@ scenario = st.selectbox(
     ("Pharmaceutical Research", "Green Chemistry", "Polymer Science")
 )
 # Configure the API key directly in the script
-API_KEY = 'AIzaSyBn8GmBfRLcjtZHz-zqQrZqzbfWaJ9n06o'
+API_KEY = 'AIzaSyABjxtyOmMGiNyC9txiONGWg2_thEZT1P8'
 genai.configure(api_key=API_KEY)
 
 # Generation configuration
