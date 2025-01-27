2d8aada28db3b4a560404287f5d100d8a287e0c6
Pravargav <96449025+Pravargav@users.noreply.github.com>
2025-01-27 21:51:12 +0530

    Update scriptfrontUi.py

--- scriptfrontUi.py
+++ scriptfrontUi.py
@@ -35,10 +35,10 @@ genai.configure(api_key=API_KEY)
 
 # Generation configuration
 generation_config = {
-    "temperature": 0.5,
+    "temperature": 0.2,
     "top_p": 0.95,
-    "top_k": 64,
-    "max_output_tokens": 8192,
+    "top_k": 32,
+    "max_output_tokens": 500,
     "response_mime_type": "text/plain",
 }
 
